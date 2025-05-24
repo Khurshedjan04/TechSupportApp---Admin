0cb1234b0c5212f64bfe5d1d4bcecd8273b3f4ea
user <alimmamadovxurshed04@gmail.com>
2025-05-24 12:08:13 +0500

    Testing porpuses

--- app/hooks/checkAuth.js
+++ app/hooks/checkAuth.js
@@ -6,35 +6,36 @@ import { logout, loginSuccess } from "../store/slices/authSlice";
 import { authAPI } from "../services/api";
 
 const useAuthCheck = () => {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+  // const dispatch = useDispatch();
+  // const router = useRouter();
+  // const { isAuthenticated, loading } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      const token = localStorage.getItem("token");
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     const token = localStorage.getItem("token");
 
-      if (!token) {
-        router.push("/login");
-        return;
-      }
+  //     if (!token) {
+  //       router.push("/login");
+  //       return;
+  //     }
 
-      try {
-        const user = await authAPI.getMe();
-        if (user.role === "user") {
-          alert("You do not have access to Admin Panel");
-          dispatch(logout());
-          return;
-        }
-        dispatch(loginSuccess({ user, token }));
-      } catch (err) {
-        dispatch(logout());
-        router.push("/login");
-      }
-    };
+  //     try {
+  //       const user = await authAPI.getMe();
+  //       if (user.role === "user") {
+  //         alert("You do not have access to Admin Panel");
+  //         dispatch(logout());
+  //         return;
+  //       }
+  //       dispatch(loginSuccess({ user, token }));
+  //     } catch (err) {
+  //       dispatch(logout());
+  //       router.push("/login");
+  //     }
+  //   };
 
-    checkAuth();
-  }, [dispatch, router]);
+  //   checkAuth();
+  // }, [dispatch, router]);
+  console.log("useAuthCheck is not running");
 };
 
 export default useAuthCheck;
